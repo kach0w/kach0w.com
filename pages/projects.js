3f1703e2b7afbcf8c5be5339981fccd30d06ed36
Kachow <karsab343@gmail.com>
2023-07-27 17:59:08 -0700

    apod

--- pages/projects.js
+++ pages/projects.js
@@ -22,6 +22,13 @@ export default function Home() {
         Some selected projects, you can find more on my <a className=' text-[#3b82f6] hover:underline' href="https://github.com/kach0w">GitHub</a>.
       </div>
       <div className='mx-auto w-[25rem] sm:w-[70rem] grid grid-cols-1 align-left sm:grid-cols-3 mt-[5rem]'>
+        <a href="https://apod-email.vercel.app/">
+          <div className='w-[20rem] group rounded-md shadow-[0_1px_4px_rgba(0,0,0,0.16)] mx-auto p-3 mb-[3vh] text-left'>
+            <p className='font-bold group-hover:underline text-slate-900'>apod-email</p>
+            <p className='text-slate-600'>See the stars! - in a weekly email</p>
+            <p className=' mt-2 text-sm text-slate-400'>NodeJS · Firebase</p>
+          </div>  
+        </a>
         <a href="https://rate-my-aps.vercel.app">
           <div className='w-[20rem] group rounded-md shadow-[0_1px_4px_rgba(0,0,0,0.16)] mx-auto p-3 mb-[3vh] text-left'>
             <p className='font-bold group-hover:underline text-slate-900'>rate-my-aps</p>
