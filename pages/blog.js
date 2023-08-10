0a0413269650b29433df240fa9cd2b83a583dfb5
Kachow <karsab343@gmail.com>
2023-08-10 01:41:50 -0700

    foot

--- pages/blog.js
+++ pages/blog.js
@@ -87,16 +87,16 @@ export default function Home() {
         </a>
       </div>
       
-        <div className='w-[25rem] mx-auto text-slate-500 text-sm font-normal'>
-          <div class="float-left">
-              <p>Fremont, California</p>
-          </div>
-          <div class="float-right">
-              <a className='text-[#3b82f6] hover:underline' href="https://github.com/kach0w">GitHub </a>·
-              {/* <a className='hover:text-slate-300' href="https://github.com/kach0w"><i class="fab fa-github"></i> GitHub</a> ·  */}
-              <a className=' text-[#3b82f6] hover:underline' href="mailto:kachow@duck.com"> Email</a>
-          </div>
+      <div className='w-[25rem] mx-auto text-slate-500 text-sm font-normal'>
+        <div class="float-left">
+            <p>Fremont, California</p>
         </div>
+        <div class="float-right">
+            <a className='text-[#3b82f6] hover:underline' href="https://github.com/kach0w">GitHub </a>·
+            {/* <a className='hover:text-slate-300' href="https://github.com/kach0w"><i class="fab fa-github"></i> GitHub</a> ·  */}
+            <a className=' text-[#3b82f6] hover:underline' href="mailto:kachow@duck.com"> Email</a>
+        </div>
+      </div>
 
 
     </div>
